d1609c00dc2aa328c0c263601a7589635f06e67c
junek3 <rlarudrms28@gmail.com>
2018-07-30 22:44:06 +0900

    Foreign Keys, ManyToMany Fields

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -9,9 +10,18 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 class Image(TimeStampedModel):
+    """ Image Model """
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
 
 class Comment(TimeStampedModel):
+    """ Comment Model """
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+class Like(TimeStampedModel):
+    """ Like Model """
+    creater = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
